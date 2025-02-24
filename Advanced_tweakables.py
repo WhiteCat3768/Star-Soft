1182a7cbb0d0181008f455b237f8044fb5cd5c41
WhiteCat3768 <142159523+WhiteCat3768@users.noreply.github.com>
2025-02-25 00:36:35 +0300

    Add files via upload

---
+++ Advanced_tweakables.py
@@ -0,0 +1,4 @@
+class Advancedtweakbles():  # Класс, содержащий специфические параметры симуляции.
+
+    Neutrino_spectrum_time = 1e-200  # Point in time in years from beginning of simulation at which you want to see a neutrino spectrum graph.
+
